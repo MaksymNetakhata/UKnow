9829eacfe74023e6de54277e56e69349c1822613
MaksymNetakhata <netakhata.maksym1121@vu.cdu.edu.ua>
2024-05-24 17:26:08 +0300

    1

---
+++ webapi/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UKnow.Data;
+
+namespace webapi.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TestController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("test")]
+        public async Task<ActionResult<IEnumerable<Test>>> GetTests()
+        {
+            return Ok(await _context.Test.ToListAsync());
+        }
+    }
+}
