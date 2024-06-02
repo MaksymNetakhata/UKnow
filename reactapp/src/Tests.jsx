1131f336c9d9ccaabde50cee4f582f0d60d5fd92
lerio01 <kovalenkolera11@gmail.com>
2024-06-02 14:14:36 +0300

    changes

--- reactapp/src/Tests.jsx
+++ reactapp/src/Tests.jsx
@@ -3,31 +3,33 @@ import React, { useEffect, useState } from 'react';
 const Test = () => {
     const [tests, setTests] = useState([]);
     const [error, setError] = useState(null);
-
-    useEffect(() => {
         const fetchTests = async () => {
-            try {
-                const response = await fetch('https://localhost:5173/test', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                });
-                if (response.ok) {
-                    throw new Error(`HTTP good! status: ${response.status}`);
-                }
-            } catch (error) {
-                setError(error);
-                console.error('Error:', error);
-            }
-        };
+            const response = {
+                method: 'GET',
+                mode: 'no-cors'
+            };
 
-        fetchTests();
-    }, []);
+            const result = await fetch('test', response);
+
+            if (result.ok ) {
+                const data = await result.json();
+                setTests(data);
+                return data;
+            } 
+            return [];
+        }
+                
+                
+useEffect(()=>{ 
+    fetchTests();
+},[]);
+       
+  
 
     return (
         <div>
             <h1>Test List</h1>
+            {error && <p>{error}</p>}
             <ul>
                 {tests.map((test) => (
                     <li key={test.id}>
