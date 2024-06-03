5f94bfaa869b010ac16131cfb0b25790af9d1696
lerio01 <kovalenkolera11@gmail.com>
2024-06-03 13:08:37 +0300

    new features tests

--- reactapp/src/Quiz.jsx
+++ reactapp/src/Quiz.jsx
@@ -9,8 +9,8 @@ const Quiz = () => {
             <div className="text">
                 <h1 className="quiz-title">Оберіть тест:</h1>
                 <ul className="test-cont">
-                    <li><Link to="/Tests">Тест 1</Link></li>
-                    <li><Link to="/quiz2">Тест 2</Link></li>
+                    <li><Link to="/Tests">Тест рівень А2</Link></li>
+                    <li><Link to="/quiz2"></Link></li>
                 </ul>
             </div>
         </div>
--- reactapp/src/Tests.jsx
+++ reactapp/src/Tests.jsx
@@ -1,9 +1,12 @@
 import {useEffect, useState} from 'react';
 import { fetchTest} from "./services/fetchTest"
+import './Tests.css';
 
 const Test = () => {
     const [testData, setTestData] = useState(null);
     const [error, setError] = useState(null);
+    const [answers, setAnswers] = useState({});
+    const [score, setScore] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -16,29 +19,82 @@ const Test = () => {
         };
         fetchData();
     }, []);
-    
+    const handleAnswerChange = (questionId, answer) => {
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            [questionId]: answer
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let correctAnswersCount = 0;
+        testData.forEach(test => {
+            const userAnswer = answers[test.id];
+            const correctAnswer = test.correctAnswer;
+            if (userAnswer === correctAnswer) {
+                correctAnswersCount++;
+            }
+        });
+        setScore(correctAnswersCount);
+    };
     return (
-        <div>
-            <h1>Test List</h1>
-            {error && <p>{error}</p>}
-            {testData ? (
-                <ul>
-                    {testData.map((test) => (
-                        <li key={test.id}>
-                            <h2>{test.question}</h2>
-                            <ul>
-                                <li>{test.option1}</li>
-                                <li>{test.option2}</li>
-                                <li>{test.option3}</li>
-                            </ul>
-                            <p>Correct Answer: {test.CorrectAnswer}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <h1>Test List</h1>
+                {error && <p>{error}</p>}
+                {testData ? (
+                    <ul>
+                        {testData.map((test) => (
+                            <li key={test.id} className="test-item">
+                                <h2>{test.question}</h2>
+                                <ul className="options">
+                                    <li>
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                name={`question-${test.id}`}
+                                                value={test.option1}
+                                                onChange={() => handleAnswerChange(test.id, test.option1)}
+                                            />
+                                            {test.option1}
+                                        </label>
+                                    </li>
+                                    <li>
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                name={`question-${test.id}`}
+                                                value={test.option2}
+                                                onChange={() => handleAnswerChange(test.id, test.option2)}
+                                            />
+                                            {test.option2}
+                                        </label>
+                                    </li>
+                                    <li>
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                name={`question-${test.id}`}
+                                                value={test.option3}
+                                                onChange={() => handleAnswerChange(test.id, test.option3)}
+                                            />
+                                            {test.option3}
+                                        </label>
+                                    </li>
+                                </ul>
+                              
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+            <button type="submit">Check
+                {score !== null && <p>Your score: {score} out of {testData.length}</p>}
+            </button>
+           
+        </form>
     );
 };
 
