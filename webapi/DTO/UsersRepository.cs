a02e417fefe9d8d45892f1f08185d27329d7c286
MaksymNetakhata <netakhata.maksym1121@vu.cdu.edu.ua>
2024-06-12 19:16:23 +0300

    usersrep

---
+++ webapi/DTO/UsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using UKnow.Data;
+using webapi.Interfaces;
+
+namespace webapi.DTO
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly AppDbContext _context;
+        public UsersRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetByEmail(string email)
+        {
+            var userEntity = await _context.User.AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email) ?? throw new Exception();
+
+            return userEntity;
+        }
+    }
+}
