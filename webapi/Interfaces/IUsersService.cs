e116576830f96bf203a71342e5621f0b913d5395
MaksymNetakhata <netakhata.maksym1121@vu.cdu.edu.ua>
2024-06-12 19:12:00 +0300

    Iservice

--- webapi/Interfaces/IUsersService.cs
+++ webapi/Interfaces/IUsersService.cs
@@ -2,5 +2,8 @@
 {
     public interface IUsersService
     {
+        Task Register(string userName, string email, string password);
+        Task<string> Login(string email, string password);
+
     }
 }
