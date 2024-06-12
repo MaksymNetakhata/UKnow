7252eb51d05de41bd0c7894f8a39bb4919269344
MaksymNetakhata <netakhata.maksym1121@vu.cdu.edu.ua>
2024-06-12 19:15:15 +0300

    profcontr

---
+++ webapi/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UKnow.Data;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfilleController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProfilleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<Profile>>> GetInfo()
+        {
+            return Ok(await _context.Profile.ToListAsync());
+        }
+    }
+}
